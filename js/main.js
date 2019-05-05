3e5b82213ca12d7dd994104a89adfd0658aa2b9c
jots <joe@vpop.net>
2019-05-05 12:10:09 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
   get_all_recs = async function (domain) {
     var i, len, type, types;
     types = ["A", "NS", "MX", "TXT"];
-    for (i = 0, len = types.length; i < len; i++) {if (window.CP.shouldStopExecution(0)) break;
+    for (i = 0, len = types.length; i < len; i++) {
       type = types[i];
       console.log(type);
       await fetch(`https://dns.google.com/query?name=yahoo.com&type=${type}`);
-    }window.CP.exitedLoop(0);
+    }
     return data;
   };
 
